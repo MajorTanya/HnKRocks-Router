1f27491dd2f6b0993f543723ae4edeb3690d7805
MajorTanya <github@majortanya.eu>
2024-02-06 02:19:37 +0100

    Fix blank page on opening direct page in browser
    Fixed by using a 0 delay refresh meta tag in the header.
    
    Increased hnkrocks-chapter-router version to 2.8.2

--- src/handlers/handleDirectPages.ts
+++ src/handlers/handleDirectPages.ts
@@ -51,16 +51,18 @@ const generateDirectPageResponse = async (chapterNo: number, pageNo: number, env
 
     const uploader = (await env.ChapterToUploader.get(`${chapterNo}`)) || 'Unknown';
 
+    const pageUrl = `https://hnk.rocks/c/${chapterNo}/p/${pageNo}` as const;
     return `<!DOCTYPE html>
     <html lang='en'>
     <head>
         <title>Chapter ${chapterNo}, Page ${pageNo} of Houseki no Kuni (by ${uploader} on MangaDex)</title>
         <meta name='twitter:card' content='summary_large_image'>
         <meta name='twitter:title' content='Houseki no Kuni - Chapter ${chapterNo}, Page ${pageNo}'>
-        <meta name='twitter:description' content='Houseki no Kuni - Chapter ${chapterNo}, Page ${pageNo}\n(by ${uploader} on <a href="https://mangadex.org">MangaDex</a>)'>
+        <meta name='twitter:description' content='Houseki no Kuni - Chapter ${chapterNo}, Page ${pageNo}\n(by ${uploader} on [MangaDex](https://mangadex.org))'>
         <meta name='twitter:image' content='https://pages.hnk.rocks/${key}'>
-        <meta name='twitter:url' content='https://hnk.rocks/c/${chapterNo}/p/${pageNo}'>
+        <meta name='twitter:url' content='${pageUrl}'>
         <meta name='twitter:site' content='HnK.Rocks'>
         <link rel='alternate' type='application/json+oembed' href='https://hnk.rocks/oembed?embed_type=${OEMBED_PAGE_DIRECT}&format=json'>
+        <meta http-equiv='Refresh' content='0; URL=${pageUrl}'>
     </head>`;
 };
